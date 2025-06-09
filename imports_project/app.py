b950258647061b64cf355cd365fab56711d5e252
systemnerd <nithin_sastry@outlook.com>
2025-06-08 20:20:42 -0700

    file operations as modules.

---
+++ imports_project/app.py
@@ -0,0 +1,4 @@
+from utils.file_operations import save_to_file, read_file
+
+save_to_file("lol", "data.txt")
+print(read_file("data.txt"))
---
+++ imports_project/utils/file_operations.py
@@ -0,0 +1,7 @@
+def save_to_file(content, file_name):
+    with open(file_name, "w") as file:
+        file.write(content)
+
+def read_file(file_name):
+    with open(file_name, "r") as file:
+        return file.read()
