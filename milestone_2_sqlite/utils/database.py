e7b9cffbc233d5c8101f7b228ba1c3f1ff972da5
systemnerd <nithin_sastry@outlook.com>
2025-06-12 21:01:46 -0700

    added type hiting

--- milestone_2_sqlite/utils/database.py
+++ milestone_2_sqlite/utils/database.py
@@ -1,18 +1,19 @@
+from typing import List, Dict, Union
 from .database_connection import DatabaseConnection
 
 """
 concerened with storing and retrieving books from a sqlite database.
 
 """
+Book = Dict[str, Union[str, int]]
 
-
-def create_book_table():
+def create_book_table() -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
 
 
-def get_all_books():
+def get_all_books() -> List[Book]:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
@@ -21,21 +22,21 @@ def get_all_books():
     return books
 
 
-def add_book(name, author):
+def add_book(name: str, author: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
         cursor.execute("INSERT INTO books VALUES(?,?,0)", (name, author))
 
 
-def mark_book_as_read(book_name):
+def mark_book_as_read(book_name: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
         cursor.execute("UPDATE books SET read=1 WHERE name=?", (book_name,))
 
 
-def delete_book(book_name):
+def delete_book(book_name: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
