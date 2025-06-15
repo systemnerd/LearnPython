8f16f2757232be8b6b9626f214d6fdf7a1601a92
systemnerd <nithin_sastry@outlook.com>
2025-06-14 21:45:42 -0700

    logging using filename

--- logging/logging_example.py
+++ logging/logging_example.py
@@ -1,7 +1,10 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(message)s')
-logger = logging.getLogger('test_logger')
+logging.basicConfig(level=logging.DEBUG, 
+                    format='%(asctime)s:%(message)s',
+                    filename='logs.txt')
+logger = logging.getLogger(__name__)
 
 logger.info("This is an INFO message")
 logger.warning("Warning message")
+logger.warning("Another message")
